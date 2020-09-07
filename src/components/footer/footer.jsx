6971ccc450a71926b89c4a78116a8082feda196a
HWolo <70845069+HWolo@users.noreply.github.com>
2020-09-07 15:51:27 +0100

    Add files via upload

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -20,7 +20,6 @@ const store = Store.store
 const styles = theme => ({
   footer: {
     //position: 'absolute',
-    marginBottom: "-30px",
     top: '0px',
     padding: '24px',
     display: 'flex',
@@ -36,12 +35,16 @@ const styles = theme => ({
     }
   },
   footerLinks: {
-    display: 'flex',
+    display: 'block',
     flexWrap: 'wrap',
     flexDirection: 'row',
-    width: '100%'
+    width: '100%',
+    textAlign: 'center'
   },
   footerText: {
+    display: 'inline-block',
+    fontSize: '15px',
+    color: 'rgb(170, 149, 133)',
     cursor: 'pointer'
   },
   languageContainer: {
@@ -78,31 +81,44 @@ class Footer extends Component {
     return (
       <div className={classes.footer}>
         <div className={classes.footerLinks}>
-          <Link to={"/"} className={ classes.link }>
+          <a href={"https://t.me/toastfinance"} target="_blank" className={ classes.link }>
             <Typography className={ classes.footerText } variant={ 'h6'}>
-              <img alt="Toast.finance" src="/icon.png" height="24" style={{marginBottom: "-4px"}} />
-              &nbsp;Toast.finance
-            </Typography>
-          </Link>
-          <br />
-          <Link to={"/staking"} className={ classes.link }>
-            <Typography className={ classes.footerText } variant={ 'h6'}>
-              Snacks
-            </Typography>
-          </Link>
-          <a href={"https://medium.com/@toastfinance/toast-finance-house-or-avo-5066766b2901"} target="_blank" className={ classes.link }>
-            <Typography className={ classes.footerText } variant={ 'h6'}>
-              Announcement
+              Telegram
             </Typography>
           </a>
-          <a href={"https://toast.finance/calc/"} target="_blank" className={ classes.link }>
+          <a href={"https://github.com/Toast-finance/"} target="_blank" className={ classes.link }>
             <Typography className={ classes.footerText } variant={ 'h6'}>
-              Calculator
+              GitHub
             </Typography>
           </a>
-          <a href={"https://gov.toast.finance/"} target="_blank" className={ classes.link }>
+          <a href={"https://twitter.com/toastfinance"} target="_blank" className={ classes.link }>
             <Typography className={ classes.footerText } variant={ 'h6'}>
-              Governance
+              Twitter
+            </Typography>
+          </a>
+          <a href={"https://medium.com/@toastfinance/"} target="_blank" className={ classes.link }>
+            <Typography className={ classes.footerText } variant={ 'h6'}>
+              Medium
+            </Typography>
+          </a>
+          <a href={"https://etherscan.io/token/0x19810559df63f19cfe88923313250550edadb743"} target="_blank" className={ classes.link }>
+            <Typography className={ classes.footerText } variant={ 'h6'}>
+              HOUSE on Etherscan
+            </Typography>
+          </a>
+          <a href={"https://etherscan.io/token/0x774adc647a8d27947c8d7c098cdb4cdf30b126de"} target="_blank" className={ classes.link }>
+            <Typography className={ classes.footerText } variant={ 'h6'}>
+              AVO on Etherscan
+            </Typography>
+          </a>
+          <a href={"https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x19810559df63f19cfe88923313250550edadb743"} target="_blank" className={ classes.link }>
+            <Typography className={ classes.footerText } variant={ 'h6'}>
+              ETH-HOUSE on Uniswap
+            </Typography>
+          </a>
+          <a href={"https://uniswap.exchange/swap?inputCurrency=0x19810559df63f19cfe88923313250550edadb743&outputCurrency=0x774adc647a8d27947c8d7c098cdb4cdf30b126de"} target="_blank" className={ classes.link }>
+            <Typography className={ classes.footerText } variant={ 'h6'}>
+              HOUSE-AVO on Uniswap
             </Typography>
           </a>
         </div>
